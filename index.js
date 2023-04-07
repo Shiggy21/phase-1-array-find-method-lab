876112feef5a7f811bc0930ecfbf512ade2a9929
Shane Jampole <shane.jampole.98@gmail.com>
2023-04-07 15:00:53 -0400

    committing labwork

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(array){
+    function isWin(year){
+        if (year.result === "W"){
+            return year
+        }
+    }
+    let didWin = array.find(isWin)
+    if (didWin){
+        return didWin.year
+    }
+    else{
+        return undefined
+    }
+
+}
